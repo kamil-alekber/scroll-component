a038cb79af0a3c47f4e8bcb85f92c806a8f7ebe8
kalekberov <kamil.alekberov@ps.kz>
2020-10-12 18:52:26 +0600

    fix: css issue

--- src/scrollComponent.tsx
+++ src/scrollComponent.tsx
@@ -1,152 +1,170 @@
-import React, { useState, useCallback, useEffect, useRef, CSSProperties } from "react";
-import styles from "./scrollComponent.css";
+import React, {
+  CSSProperties,
+  useState,
+  useCallback,
+  useEffect,
+  useRef
+} from 'react'
+import styles from './scrollComponent.module.css'
 
-const SCROLL_BOX_MIN_HEIGHT = 20;
+const SCROLL_BOX_MIN_HEIGHT = 20
 
-export interface ScrollComponentProps{
-  children: React.ReactNode;
+export interface ScrollComponentProps {
+  children: React.ReactNode
   showScroll?: boolean
-  scrollContainerClass?:string;
-  scrollContainerStyles?: CSSProperties,
-  scrollHostStyles?: CSSProperties,
-  scrollBarStyles?:CSSProperties,
-  scrollThumbStyles?:CSSProperties
+  scrollContainerClass?: string
+  scrollContainerStyles?: CSSProperties
+  scrollHostStyles?: CSSProperties
+  scrollBarStyles?: CSSProperties
+  scrollThumbStyles?: CSSProperties
 }
 
-export function ScrollComponent({ children, showScroll, scrollBarStyles, scrollContainerStyles,scrollHostStyles,scrollThumbStyles, scrollContainerClass}: ScrollComponentProps) {
-  const [hovering, setHovering] = useState(false);
-  const [scrollBoxHeight, setScrollBoxHeight] = useState(SCROLL_BOX_MIN_HEIGHT);
-  const [scrollBoxTop, setScrollBoxTop] = useState(0);
-  const [lastScrollThumbPosition, setScrollThumbPosition] = useState(0);
-  const [isDragging, setDragging] = useState(false);
+export function ScrollComponent({
+  children,
+  showScroll,
+  scrollBarStyles,
+  scrollContainerStyles,
+  scrollHostStyles,
+  scrollThumbStyles,
+  scrollContainerClass
+}: ScrollComponentProps) {
+  const [hovering, setHovering] = useState(false)
+  const [scrollBoxHeight, setScrollBoxHeight] = useState(SCROLL_BOX_MIN_HEIGHT)
+  const [scrollBoxTop, setScrollBoxTop] = useState(0)
+  const [lastScrollThumbPosition, setScrollThumbPosition] = useState(0)
+  const [isDragging, setDragging] = useState(false)
 
   const handleMouseOver = useCallback(() => {
-    !hovering && setHovering(true);
-  }, [hovering]);
+    !hovering && setHovering(true)
+  }, [hovering])
 
   const handleMouseOut = useCallback(() => {
-    !!hovering && setHovering(false);
-  }, [hovering]);
+    !!hovering && setHovering(false)
+  }, [hovering])
 
   const handleDocumentMouseUp = useCallback(
-    e => {
+    (e) => {
       if (isDragging) {
-        e.preventDefault();
-        setDragging(false);
+        e.preventDefault()
+        setDragging(false)
       }
     },
     [isDragging]
-  );
+  )
 
   const handleDocumentMouseMove = useCallback(
-    e => {
+    (e) => {
       if (isDragging) {
-        e.preventDefault();
-        e.stopPropagation();
-        const scrollHostElement = scrollHostRef.current;
-        if(scrollHostElement){
+        e.preventDefault()
+        e.stopPropagation()
+        const scrollHostElement = scrollHostRef.current
+        if (scrollHostElement) {
+          const { scrollHeight, offsetHeight } = scrollHostElement
 
-          const { scrollHeight, offsetHeight } = scrollHostElement;
+          const deltaY = e.clientY - lastScrollThumbPosition
+          const percentage = deltaY * (scrollHeight / offsetHeight)
 
-        let deltaY = e.clientY - lastScrollThumbPosition;
-        let percentage = deltaY * (scrollHeight / offsetHeight);
-
-        setScrollThumbPosition(e.clientY);
-        setScrollBoxTop(
-          Math.min(
-            Math.max(0, scrollBoxTop + deltaY),
-            offsetHeight - scrollBoxHeight
+          setScrollThumbPosition(e.clientY)
+          setScrollBoxTop(
+            Math.min(
+              Math.max(0, scrollBoxTop + deltaY),
+              offsetHeight - scrollBoxHeight
+            )
           )
-          );
-        scrollHostElement.scrollTop = Math.min(
-          scrollHostElement.scrollTop + percentage,
-          scrollHeight - offsetHeight
-          );
+          scrollHostElement.scrollTop = Math.min(
+            scrollHostElement.scrollTop + percentage,
+            scrollHeight - offsetHeight
+          )
         }
       }
     },
     [isDragging, lastScrollThumbPosition, scrollBoxHeight, scrollBoxTop]
-  );
+  )
 
-  const handleScrollThumbMouseDown = useCallback(e => {
-    e.preventDefault();
-    e.stopPropagation();
-    setScrollThumbPosition(e.clientY);
-    setDragging(true);
-    console.log("handleScrollThumbMouseDown");
-  }, []);
+  const handleScrollThumbMouseDown = useCallback((e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    setScrollThumbPosition(e.clientY)
+    setDragging(true)
+    console.log('handleScrollThumbMouseDown')
+  }, [])
 
   const handleScroll = useCallback(() => {
     if (!scrollHostRef) {
-      return;
+      return
     }
-    const scrollHostElement = scrollHostRef.current;
-    if(scrollHostElement){
+    const scrollHostElement = scrollHostRef.current
+    if (scrollHostElement) {
+      const { scrollTop, scrollHeight, offsetHeight } = scrollHostElement
 
-      const { scrollTop, scrollHeight, offsetHeight } = scrollHostElement;
-
-      let newTop = (scrollTop / scrollHeight) * offsetHeight;
+      let newTop = (scrollTop / scrollHeight) * offsetHeight
       // newTop = newTop + parseInt(scrollTop, 10);
-      newTop = Math.min(newTop, offsetHeight - scrollBoxHeight);
-      setScrollBoxTop(newTop);
+      newTop = Math.min(newTop, offsetHeight - scrollBoxHeight)
+      setScrollBoxTop(newTop)
     }
-  }, []);
+  }, [])
 
-  const scrollHostRef = useRef<HTMLDivElement>(null);
+  const scrollHostRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const scrollHostElement = scrollHostRef.current;
-    if(scrollHostElement){
-      const { clientHeight, scrollHeight } = scrollHostElement;
-      const scrollThumbPercentage = clientHeight / scrollHeight;
+    const scrollHostElement = scrollHostRef.current
+    if (scrollHostElement) {
+      const { clientHeight, scrollHeight } = scrollHostElement
+      const scrollThumbPercentage = clientHeight / scrollHeight
       const scrollThumbHeight = Math.max(
         scrollThumbPercentage * clientHeight,
         SCROLL_BOX_MIN_HEIGHT
-    );
-    setScrollBoxHeight(scrollThumbHeight);
-    scrollHostElement.addEventListener("scroll", handleScroll, true);
-  }
+      )
+      setScrollBoxHeight(scrollThumbHeight)
+      scrollHostElement.addEventListener('scroll', handleScroll, true)
+    }
     return function cleanup() {
-      if(scrollHostElement){
-        scrollHostElement.removeEventListener("scroll", handleScroll, true);
+      if (scrollHostElement) {
+        scrollHostElement.removeEventListener('scroll', handleScroll, true)
       }
-    };
-  }, []);
+    }
+  }, [])
 
   useEffect(() => {
-    //this is handle the dragging on scroll-thumb
-    document.addEventListener("mousemove", handleDocumentMouseMove);
-    document.addEventListener("mouseup", handleDocumentMouseUp);
-    document.addEventListener("mouseleave", handleDocumentMouseUp);
+    // this is handle the dragging on scroll-thumb
+    document.addEventListener('mousemove', handleDocumentMouseMove)
+    document.addEventListener('mouseup', handleDocumentMouseUp)
+    document.addEventListener('mouseleave', handleDocumentMouseUp)
     return function cleanup() {
-      document.removeEventListener("mousemove", handleDocumentMouseMove);
-      document.removeEventListener("mouseup", handleDocumentMouseUp);
-      document.removeEventListener("mouseleave", handleDocumentMouseUp);
-    };
-  }, [handleDocumentMouseMove, handleDocumentMouseUp]);
+      document.removeEventListener('mousemove', handleDocumentMouseMove)
+      document.removeEventListener('mouseup', handleDocumentMouseUp)
+      document.removeEventListener('mouseleave', handleDocumentMouseUp)
+    }
+  }, [handleDocumentMouseMove, handleDocumentMouseUp])
 
   return (
     <div
-      className={`${styles["scrollhost-container"]} ${scrollContainerClass}`}
+      className={`${styles['scrollhost-container']} ${scrollContainerClass}`}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
       style={scrollContainerStyles}
-      >
+    >
       <div
         ref={scrollHostRef}
         className={styles['scrollhost']}
         style={scrollHostStyles}
-        >
+      >
         {children}
       </div>
       <div
-        className={styles["scroll-bar"]} style={{ opacity: showScroll || hovering ? 1 : 0, ...scrollBarStyles }}>
+        className={styles['scroll-bar']}
+        style={{ opacity: showScroll || hovering ? 1 : 0, ...scrollBarStyles }}
+      >
         <div
-          className={styles["scroll-thumb"]}
-          style={{ height: scrollBoxHeight, top: scrollBoxTop, ...scrollThumbStyles }}
+          className={styles['scroll-thumb']}
+          style={{
+            height: scrollBoxHeight,
+            top: scrollBoxTop,
+            ...scrollThumbStyles
+          }}
           onMouseDown={handleScrollThumbMouseDown}
         />
       </div>
-  </div>
-  );
+    </div>
+  )
 }
