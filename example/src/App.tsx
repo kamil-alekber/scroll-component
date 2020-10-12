1ddb512489b2cb9848b911645c08f12a3a9212b6
kalekberov <kamil.alekberov@ps.kz>
2020-10-12 16:28:24 +0600

    fix: add class props to a scroll container

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,52 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-scroll'
+import { ScrollComponent } from 'react-scroll'
 import 'react-scroll/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return(
+
+    <ScrollComponent showScroll scrollContainerClass="my-class" scrollContainerStyles={{height: 300, width: 250 ,margin: '0 auto'}} >
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fuga inventore sit nulla officia repellat recusandae quam! Iste cupiditate vitae quibusdam, ipsam inventore nostrum consequatur perspiciatis autem dolorum consequuntur esse!</p>
+  </ScrollComponent>
+    )
 }
 
 export default App
---
+++ src/scrollComponent.tsx
@@ -0,0 +1,152 @@
+import React, { useState, useCallback, useEffect, useRef, CSSProperties } from "react";
+import styles from "./scrollComponent.css";
+
+const SCROLL_BOX_MIN_HEIGHT = 20;
+
+export interface ScrollComponentProps{
+  children: React.ReactNode;
+  showScroll?: boolean
+  scrollContainerClass?:string;
+  scrollContainerStyles?: CSSProperties,
+  scrollHostStyles?: CSSProperties,
+  scrollBarStyles?:CSSProperties,
+  scrollThumbStyles?:CSSProperties
+}
+
+export function ScrollComponent({ children, showScroll, scrollBarStyles, scrollContainerStyles,scrollHostStyles,scrollThumbStyles, scrollContainerClass}: ScrollComponentProps) {
+  const [hovering, setHovering] = useState(false);
+  const [scrollBoxHeight, setScrollBoxHeight] = useState(SCROLL_BOX_MIN_HEIGHT);
+  const [scrollBoxTop, setScrollBoxTop] = useState(0);
+  const [lastScrollThumbPosition, setScrollThumbPosition] = useState(0);
+  const [isDragging, setDragging] = useState(false);
+
+  const handleMouseOver = useCallback(() => {
+    !hovering && setHovering(true);
+  }, [hovering]);
+
+  const handleMouseOut = useCallback(() => {
+    !!hovering && setHovering(false);
+  }, [hovering]);
+
+  const handleDocumentMouseUp = useCallback(
+    e => {
+      if (isDragging) {
+        e.preventDefault();
+        setDragging(false);
+      }
+    },
+    [isDragging]
+  );
+
+  const handleDocumentMouseMove = useCallback(
+    e => {
+      if (isDragging) {
+        e.preventDefault();
+        e.stopPropagation();
+        const scrollHostElement = scrollHostRef.current;
+        if(scrollHostElement){
+
+          const { scrollHeight, offsetHeight } = scrollHostElement;
+
+        let deltaY = e.clientY - lastScrollThumbPosition;
+        let percentage = deltaY * (scrollHeight / offsetHeight);
+
+        setScrollThumbPosition(e.clientY);
+        setScrollBoxTop(
+          Math.min(
+            Math.max(0, scrollBoxTop + deltaY),
+            offsetHeight - scrollBoxHeight
+          )
+          );
+        scrollHostElement.scrollTop = Math.min(
+          scrollHostElement.scrollTop + percentage,
+          scrollHeight - offsetHeight
+          );
+        }
+      }
+    },
+    [isDragging, lastScrollThumbPosition, scrollBoxHeight, scrollBoxTop]
+  );
+
+  const handleScrollThumbMouseDown = useCallback(e => {
+    e.preventDefault();
+    e.stopPropagation();
+    setScrollThumbPosition(e.clientY);
+    setDragging(true);
+    console.log("handleScrollThumbMouseDown");
+  }, []);
+
+  const handleScroll = useCallback(() => {
+    if (!scrollHostRef) {
+      return;
+    }
+    const scrollHostElement = scrollHostRef.current;
+    if(scrollHostElement){
+
+      const { scrollTop, scrollHeight, offsetHeight } = scrollHostElement;
+
+      let newTop = (scrollTop / scrollHeight) * offsetHeight;
+      // newTop = newTop + parseInt(scrollTop, 10);
+      newTop = Math.min(newTop, offsetHeight - scrollBoxHeight);
+      setScrollBoxTop(newTop);
+    }
+  }, []);
+
+  const scrollHostRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scrollHostElement = scrollHostRef.current;
+    if(scrollHostElement){
+      const { clientHeight, scrollHeight } = scrollHostElement;
+      const scrollThumbPercentage = clientHeight / scrollHeight;
+      const scrollThumbHeight = Math.max(
+        scrollThumbPercentage * clientHeight,
+        SCROLL_BOX_MIN_HEIGHT
+    );
+    setScrollBoxHeight(scrollThumbHeight);
+    scrollHostElement.addEventListener("scroll", handleScroll, true);
+  }
+    return function cleanup() {
+      if(scrollHostElement){
+        scrollHostElement.removeEventListener("scroll", handleScroll, true);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    //this is handle the dragging on scroll-thumb
+    document.addEventListener("mousemove", handleDocumentMouseMove);
+    document.addEventListener("mouseup", handleDocumentMouseUp);
+    document.addEventListener("mouseleave", handleDocumentMouseUp);
+    return function cleanup() {
+      document.removeEventListener("mousemove", handleDocumentMouseMove);
+      document.removeEventListener("mouseup", handleDocumentMouseUp);
+      document.removeEventListener("mouseleave", handleDocumentMouseUp);
+    };
+  }, [handleDocumentMouseMove, handleDocumentMouseUp]);
+
+  return (
+    <div
+      className={`${styles["scrollhost-container"]} ${scrollContainerClass}`}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      style={scrollContainerStyles}
+      >
+      <div
+        ref={scrollHostRef}
+        className={styles['scrollhost']}
+        style={scrollHostStyles}
+        >
+        {children}
+      </div>
+      <div
+        className={styles["scroll-bar"]} style={{ opacity: showScroll || hovering ? 1 : 0, ...scrollBarStyles }}>
+        <div
+          className={styles["scroll-thumb"]}
+          style={{ height: scrollBoxHeight, top: scrollBoxTop, ...scrollThumbStyles }}
+          onMouseDown={handleScrollThumbMouseDown}
+        />
+      </div>
+  </div>
+  );
+}
